cf95fbe05c871d618ca49d9a48bdd3977c844d30
Javier Aliaga <reivaj_31@hotmail.com>
2019-05-11 12:38:11 -0300

    Modificación de vistas.
    Se cambian a las nuevas vistas , en sus respectivos package de administrador, supervisor y dueño.

--- src/root/gestionagricola/vistas/Login.java
+++ src/root/gestionagricola/vistas/Login.java
@@ -9,12 +9,12 @@ package root.gestionagricola.vistas;
  *
  * @author Javier
  */
-public class VistaLogin extends javax.swing.JPanel {
+public class Login extends javax.swing.JPanel {
 
     /**
      * Creates new form VistaLogiin
      */
-    public VistaLogin() {
+    public Login() {
         initComponents();
     }
 
@@ -33,7 +33,7 @@ public class VistaLogin extends javax.swing.JPanel {
         jTextField1 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         jPasswordField1 = new javax.swing.JPasswordField();
-        jLabel1 = new javax.swing.JLabel();
+        Fondo = new javax.swing.JLabel();
 
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
@@ -56,6 +56,7 @@ public class VistaLogin extends javax.swing.JPanel {
         jButton1.setFont(new java.awt.Font("Garamond", 1, 15)); // NOI18N
         jButton1.setForeground(new java.awt.Color(0, 0, 0));
         jButton1.setText("Iniciar Sesión");
+        jButton1.setFocusPainted(false);
 
         jPasswordField1.setFont(new java.awt.Font("Garamond", 1, 14)); // NOI18N
         jPasswordField1.setText("Contrasena");
@@ -98,9 +99,9 @@ public class VistaLogin extends javax.swing.JPanel {
 
         jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 100, 420, 510));
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/root/gestionagricola/vistas/imagenes/Fondo claro.jpg"))); // NOI18N
-        jLabel1.setPreferredSize(new java.awt.Dimension(1270, 693));
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1323, 735));
+        Fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/root/gestionagricola/vistas/imagenes/Fondo claro.jpg"))); // NOI18N
+        Fondo.setPreferredSize(new java.awt.Dimension(1270, 693));
+        jPanel1.add(Fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1300, 735));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -116,9 +117,9 @@ public class VistaLogin extends javax.swing.JPanel {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Fondo;
     private javax.swing.JLabel Titulo2;
     private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPasswordField jPasswordField1;
