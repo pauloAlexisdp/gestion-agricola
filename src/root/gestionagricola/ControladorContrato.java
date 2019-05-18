8a2bbeafb63a5a5217a1210538c2ffcf36aed3cc
Francisco Álvarez <fco.alvarez.08@gmail.com>
2019-05-17 21:18:40 -0400

    Estructura GestionContrato
    Estructura Base, falta la implementación.

---
+++ src/root/gestionagricola/ControladorContrato.java
@@ -0,0 +1,30 @@
+
+package root.gestionagricola;
+
+import java.util.ArrayList;
+
+/**
+ * Permite agregar funcionalidad a la gestion de reportes.
+ * @author Los Lanzas
+ */
+public class ControladorContrato {
+    
+    
+    public static void ingresarContrato(int folio, String tipo, String estado, 
+            String f_inicio, String f_termino, String nombre, int rut, int sueldo){
+        
+    }
+    
+    public ArrayList buscarContrato(int folio, String tipo, String estado, 
+            String f_inicio, String f_termino, String nombre, int rut, int sueldo){
+        return null;
+    }
+    
+    public Object getContrato(int folio){
+        return null;
+    }
+    
+    public void eliminarContrato(int folio){
+        
+    }
+}
