a65c7de539f5011181a57071cccfb4af41c6c50b
miguel <jorquera1401@gmail.com>
2019-05-18 20:12:51 -0400

    Busqueda de un usuario
    Se creo la funcion buscar cuenta que es un usuario, recibe como parametros el nombre y la contraseña desde la ventana y la consulta en CuentaDA la que retorna un objeto Cuenta para que en el controlador se pase este objeto y manejar mejor

--- src/root/gestionagricola/CuentaDA.java
+++ src/root/gestionagricola/CuentaDA.java
@@ -21,19 +21,45 @@ public class CuentaDA {
         this.cuenta = cuenta;
     }
     
+    /**
+     * guardo los datos de los usuarios en la tabla con el objeto cuenta que es pasado desde mi vista
+     * @throws ClassNotFoundException
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     * @throws SQLException 
+     */
     public void guardar() throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException{
         Conexion cdb = FactoriaConexion.getInstancia().getConexiondb();
-        cdb.un_sql = "select nombre cuenta where nombre = "+cuenta.getNombre();
+        //modulo seguridad si ya hay una cuenta con ese nombre 
+        cdb.un_sql = "select nombre from cuenta where nombre = '"+cuenta.getNombre()+"'";
         cdb.resultado = cdb.statement.executeQuery(cdb.un_sql);
         if(cdb.resultado!=null){
-            
+           if(cdb.resultado.next()){
+               cdb.un_sql = "UPDATE cuenta set contrasena='"+cuenta.getPassword()+"', tipo='"+cuenta.getTipo()+"'"+
+                       "WHERE nombre='"+cuenta.getNombre()+"'";
+                cdb.statement.executeUpdate(cdb.un_sql);
+                System.out.println("Datos actualizados");
+           }else{
+               cdb.un_sql = "Insert into cuenta values('"+cuenta.getNombre()+"','"+cuenta.getPassword()+"','"+cuenta.getTipo()+"')";
+               cdb.statement.executeUpdate(cdb.un_sql);
+               System.out.println("Datos guardados");
+               
+           }
         }else{
-            cdb.un_sql = "Insert into cuenta values ('"+cuenta.getNombre()+"','"+cuenta.getPassword()+"','"+cuenta.getTipo()+"'";
+            cdb.un_sql = "Insert into cuenta values ('"+cuenta.getNombre()+"','"+cuenta.getPassword()+"','"+cuenta.getTipo()+"')";
             cdb.statement.executeUpdate(cdb.un_sql);
         }
-    
+        cdb.close();
     }
     
+    /**
+     * cargo una lista de todos los usuarios registrados en mi cuenta
+     * @return una lista de todas las cuentas que tengo en la tabla cuenta
+     * @throws ClassNotFoundException
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     * @throws SQLException 
+     */
     public ArrayList cargar() throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException{
         ArrayList r = null;
         Cuenta c;
@@ -45,13 +71,42 @@ public class CuentaDA {
             while(cdb.resultado.next()){
                 c = new Cuenta();
                 c.setNombre(cdb.resultado.getString("nombre"));
-                c.setPassword(cdb.resultado.getString("password"));
+                c.setPassword(cdb.resultado.getString("contrasena"));
                 c.setTipo(cdb.resultado.getString("tipo"));
                 r.add(c);
             }
         }else{
             System.out.println("error");
         }
+        cdb.close();
       return r;
+    }
+    /**
+     * 
+     * @param nombre nombre de usuario
+     * @param password password para comprobar si el usuario ingresado es valido
+     * @return retorn la cuenta rescatada de la base de datos, debe ser solo una fila como respuesta
+     * @throws ClassNotFoundException
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     * @throws SQLException 
+     */
+    public Cuenta buscar(String nombre, String password) throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException{
+        
+        Conexion cdb = FactoriaConexion.getInstancia().getConexiondb();
+        
+        cdb.un_sql = "select nombre,contrasena, tipo from cuenta where nombre like '"+nombre+"' and contrasena like '"+password+"'";
+        cdb.resultado = cdb.statement.executeQuery(cdb.un_sql);
+        if(cdb.resultado!=null){
+        while(cdb.resultado.next()){
+            cuenta.setNombre(cdb.resultado.getString("nombre"));
+            cuenta.setPassword(cdb.resultado.getString("contrasena"));
+            cuenta.setTipo(cdb.resultado.getString("tipo"));
+            System.out.println("Nombre "+cuenta.getNombre()+" tipo "+cuenta.getTipo());
+        }
+        }else{
+        }
+      //  cdb.close();
+        return cuenta;
     }
 }
